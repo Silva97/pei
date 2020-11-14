0b435e881c99ab1a35c4417e979a7bf0f5b77c1b
Luiz Felipe <felipe.silva337@yahoo.com>
2020-11-14 16:34:58 -0300

    feat: implement utils for search zero sequences on sections

--- include/pereader.h
+++ include/pereader.h
@@ -2,13 +2,23 @@
 #define _PEREADER_H
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "pe.h"
+
+typedef struct pe_block
+{
+    uint64_t offset;
+    uint64_t size;
+    uint16_t section;
+} pe_block_t;
 
 FILE *pe_open(char *filename);
 pe_t *pe_parse(FILE *executable);
+void pe_seek(pe_t *pe, long int offset);
 
 void pe_show_type(pe_t *pe);
-void pe_show_coff(pe_t *pe);
+void pe_show_coff_characteristics(pe_t *pe, bool verbose);
+void pe_show_coff(pe_t *pe, bool verbose);
 void pe32_show_optional_header(pe_t *pe);
 void pe64_show_optional_header(pe_t *pe);
 void pe_show_section_header(pe_t *pe, unsigned int section_number);
@@ -16,5 +26,7 @@ void pe_show_all_section_headers(pe_t *pe);
 
 int pe_search_address_section(pe_t *pe, uint32_t address);
 int pe_search_entrypoint_section(pe_t *pe);
+pe_block_t pe_search_biggest_zero_sequence_on_section(pe_t *pe, unsigned int section);
+pe_block_t pe_search_biggest_zero_sequence(pe_t *pe);
 
 #endif /* _PEREADER_H */
--- src/pe/pe_utils.c
+++ src/pe/pe_utils.c
@@ -1,4 +1,10 @@
+#include <stdbool.h>
 #include "pereader.h"
+
+void pe_seek(pe_t *pe, long int offset)
+{
+  fseek(pe->file, offset, SEEK_SET);
+}
 
 int pe_search_address_section(pe_t *pe, uint32_t address)
 {
@@ -26,4 +32,69 @@ int pe_search_entrypoint_section(pe_t *pe)
 
   pe64_optional_header_t *optional_header = pe->optional_header;
   return pe_search_address_section(pe, optional_header->entry_point);
+}
+
+pe_block_t pe_search_biggest_zero_sequence_on_section(pe_t *pe, unsigned int section)
+{
+  pe_block_t biggest = {
+      .offset = 0,
+      .size = 0,
+      .section = section,
+  };
+  uint64_t current_offset = 0;
+  size_t current_size = 0;
+  bool in_sequence = false;
+
+  uint32_t start_offset = pe->section_header[section]->pointer_to_raw_data;
+  pe_seek(pe, start_offset);
+
+  uint64_t i;
+  for (i = 0; i < pe->section_header[section]->size_of_raw_data; i++)
+  {
+    if (fgetc(pe->file) == '\0')
+    {
+      if (!in_sequence)
+      {
+        current_offset = start_offset + i;
+        current_size = 1;
+        in_sequence = true;
+      }
+      else
+      {
+        current_size++;
+      }
+    }
+    else if (in_sequence)
+    {
+      in_sequence = false;
+      if (current_size > biggest.size)
+      {
+        biggest.offset = current_offset;
+        biggest.size = current_size;
+      }
+    }
+  }
+
+  return biggest;
+}
+
+pe_block_t pe_search_biggest_zero_sequence(pe_t *pe)
+{
+  pe_block_t block;
+  pe_block_t biggest = {
+      .offset = 0,
+      .size = 0,
+      .section = 0,
+  };
+
+  for (int i = 0; i < pe->coff_header->number_of_sections; i++)
+  {
+    block = pe_search_biggest_zero_sequence_on_section(pe, i);
+    if (block.size > biggest.size)
+    {
+      biggest = block;
+    }
+  }
+
+  return biggest;
 }
