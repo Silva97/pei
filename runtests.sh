ac3ecb990ac0ccfa7accde71464d7d4ca682a57f
Luiz Felipe <felipe.silva337@yahoo.com>
2020-11-21 21:21:45 -0300

    test: add tests to pe_write.c module

--- runtests.sh
+++ runtests.sh
@@ -5,6 +5,7 @@ function run_tests() {
     make test name=pe_utils || exit
     make test name=pe_get_field || exit
     make test name=pe_set_field || exit
+    make test name=pe_write || exit
 }
 
 time -p run_tests
